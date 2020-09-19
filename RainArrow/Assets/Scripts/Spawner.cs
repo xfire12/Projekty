28cf8b5df293fb72a16ffe343f98300c7f35fe27
xfire12 <Braego@wp.pl>
2020-09-19 19:12:53 +0200

    halo
    new

---
+++ RainArrow/Assets/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject arrow;
+
+    private float min_X = -2.78f;
+    private float max_X = 2.78f;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(StartSpawning());
+    }
+
+    IEnumerator StartSpawning()
+    {
+        yield return new WaitForSeconds(Random.Range(0.5f, 1f));
+
+        GameObject k = Instantiate(arrow);
+
+        float x = Random.Range(min_X, max_X);
+
+        k.transform.position = new Vector2(x, transform.position.y);
+
+        StartCoroutine(StartSpawning());
+
+    }
+
+
+
+} // class
