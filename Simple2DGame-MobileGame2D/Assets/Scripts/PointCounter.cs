0f3c997cc461878d1f752f322d0e28c43c67fd5a
xfire12 <Braego@wp.pl>
2020-09-20 19:59:07 +0200

    nowe

---
+++ Simple2DGame-MobileGame2D/Assets/Scripts/PointCounter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PointCounter : MonoBehaviour
+{
+
+    public Text pointText;
+    private int points = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointText.text = "score:" + points.ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Box")
+        {
+            points += 1;
+            Destroy(collision.gameObject);
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ zajecia4/Assets/scripts/Opendoor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opendoor : MonoBehaviour
+{
+    public SpriteRenderer door;
+    public Sprite doorOpen;
+    public Sprite mySprite;
+
+    private SpriteRenderer toggle;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggle = GetComponent<SpriteRenderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   private void OnTriggerEnter2D(Collider2D other)
+   {
+       if(other.gameObject.tag =="player")
+        {
+            toggle.sprite = mySprite;
+            door.sprite = doorOpen;
+           
+
+
+        }
+
+
+
+    }
+
+
+
+
+}
